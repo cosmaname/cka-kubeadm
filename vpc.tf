54c03c7364d2bacdb52eb0ad76565ebc17a33848
Razvan Cosma <r@cosma.name>
2023-12-31 11:01:23 +0000

    just public

--- vpc.tf
+++ vpc.tf
@@ -10,12 +10,10 @@ module "vpc" {
   name                 = local.name
   cidr                 = local.vpc_cidr
   azs                  = local.azs
-  private_subnets      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
   public_subnets       = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
   enable_dns_hostnames = true
   enable_dns_support   = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway   = false
 
   tags = local.tags
 }

6e149cd530b208b6e31ce4ee966f5751115d4659
Razvan Cosma <r@cosma.name>
2023-12-31 11:28:50 +0000

    SGs

---
+++ sgs.tf
@@ -0,0 +1,62 @@
+# The AWS provider removes the default "allow all "egress rule from all security
+# groups, so it has to be defined explicitly.
+resource "aws_security_group" "egress" {
+  name        = "${local.name}-egress"
+  description = "Allow all outgoing traffic to everywhere"
+  vpc_id      = module.vpc.vpc_id
+  tags        = local.tags
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ingress_internal" {
+  name        = "${local.name}-ingress-internal"
+  description = "Allow all incoming traffic from nodes and Pods in the cluster"
+  vpc_id      = module.vpc.vpc_id
+  tags        = local.tags
+  ingress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    self        = true
+    description = "Allow incoming traffic from cluster nodes"
+
+  }
+  ingress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = var.pod_network_cidr_block != null ? [var.pod_network_cidr_block] : null
+    description = "Allow incoming traffic from the Pods of the cluster"
+  }
+}
+
+resource "aws_security_group" "ingress_k8s" {
+  name        = "${local.name}-ingress-k8s"
+  description = "Allow incoming Kubernetes API requests (TCP/6443) from outside the cluster"
+  vpc_id      = module.vpc.vpc_id
+  tags        = local.tags
+  ingress {
+    protocol    = "tcp"
+    from_port   = 6443
+    to_port     = 6443
+    cidr_blocks = var.allowed_k8s_cidr_blocks
+  }
+}
+
+resource "aws_security_group" "ingress_ssh" {
+  name        = "${local.name}-ingress-ssh"
+  description = "Allow incoming SSH traffic (TCP/22) from outside the cluster"
+  vpc_id      = module.vpc.vpc_id
+  tags        = local.tags
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = var.allowed_ssh_cidr_blocks
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "private_key_file" {
+  type        = string
+  description = "Filename of the private key of a key pair on your local machine. This key pair will allow to connect to the nodes of the cluster with SSH."
+  default     = "~/.ssh/id_ed25519"
+}
+
+variable "public_key_file" {
+  type        = string
+  description = "Filename of the public key of a key pair on your local machine. This key pair will allow to connect to the nodes of the cluster with SSH."
+  default     = "~/.ssh/id_ed25519.pub"
+}
+
+variable "allowed_ssh_cidr_blocks" {
+  type        = list(string)
+  description = "List of CIDR blocks from which it is allowed to make SSH connections to the EC2 instances that form the cluster nodes. By default, SSH connections are allowed from everywhere."
+}
+
+variable "allowed_k8s_cidr_blocks" {
+  type        = list(string)
+  description = "List of CIDR blocks from which it is allowed to make Kubernetes API request to the API server of the cluster. By default, Kubernetes API requests are allowed from everywhere. Note that Kubernetes API requests from Pods and nodes inside the cluster are always allowed, regardless of the value of this variable."
+}
+
+variable "master_instance_type" {
+  type        = string
+  description = "EC2 instance type for the master node (must have at least 2 CPUs and 2 GB RAM)."
+  default     = "t3.medium"
+}
+
+variable "worker_instance_type" {
+  type        = string
+  description = "EC2 instance type for the worker nodes."
+  default     = "t3.small"
+}
+
+variable "num_workers" {
+  type        = number
+  description = "Number of worker nodes."
+  default     = 2
+}
+
+variable "pod_network_cidr_block" {
+  type        = string
+  description = "**This is an optional variable with a default value of null**. CIDR block for the Pod network of the cluster. If set, Kubernetes automatically allocates Pod subnet IP address ranges to the nodes (i.e. sets the \".spec.podCIDR\" field of the node objects). If null, the cluster is created without an explicitly determined Pod network IP address range, and the nodes are not allocated any Pod subnet IP address ranges (i.e. the \".spec.podCIDR\" field of the nodes is not set)."
+  default     = null
+}
